ac454d68abddca56f2c8c13791003e9a00482f72
demensdeum <demensdeum>
2017-04-23 15:21:10 +0400

    Old code removed

--- DeathMask/src/Controllers/CreditsController/DMCreditsController.cpp
+++ DeathMask/src/Controllers/CreditsController/DMCreditsController.cpp
@@ -23,12 +23,6 @@ DMCreditsController::DMCreditsController(const DMCreditsController& orig) {
 }
 
 void DMCreditsController::beforeStart() {
-    
-//    auto demensdeumLogo = std::make_shared<FSEObject>();
-//    
-//    auto model = FSEGTFactory::makeModelComponent(std::make_shared<string>("./data/graphics/models/cube/cube.bam"));
-//    
-//    demensdeumLogo->addComponent(model);
   
     auto demensdeumLogo = FSEGTFactory::makeOnSceneObject(
             std::make_shared<string>("demensdeumLogo"),
