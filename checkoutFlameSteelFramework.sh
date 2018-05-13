985bb04e89f839050289489635473e5cc27921c0
demensdeum <demensdeum@gmail.com>
2018-05-13 13:00:46 +0400

    Flame Steel Framework dependencies moved to separate repositories

---
+++ checkoutFlameSteelFramework.sh
@@ -0,0 +1,8 @@
+mkdir FlameSteelFramework 
+cd FlameSteelFramework
+git clone https://github.com/demensdeum/FlameSteelCore.git
+git clone https://github.com/demensdeum/FlameSteelEngineGameToolkit.git
+git clone https://github.com/demensdeum/FlameSteelEngineGameToolkitAlgorithms.git
+git clone https://github.com/demensdeum/FlameSteelEngineGameToolkitFSGL.git
+git clone https://github.com/demensdeum/FlameSteelSpiderToolkit.git
+git clone https://github.com/demensdeum/FSGL.git
