0b632e6e20462ac0691e891c8cebb8e49fc21abc
Demensdeum <demensdeum@gmail.com>
2018-10-03 23:07:34 +0400

    Emscripten specific changes

--- emscriptenBuild.sh
+++ emscriptenBuild.sh
@@ -1,2 +1,8 @@
+rm -rf data
+cp -R DeathMask/data data
+cd data
+mogrify -filter point -resize 256x256\! *.bmp
+cd ..
 source ../emsdk/emsdk_env.sh
 cmake -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake && make
+rm -rf data
