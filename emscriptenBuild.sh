ffd151a1360903682e3634982d42f314221cec2a
Demensdeum <demensdeum@gmail.com>
2018-10-10 19:26:39 +0400

    mp3 added for browser player

--- emscriptenBuild.sh
+++ emscriptenBuild.sh
@@ -6,7 +6,7 @@ rm -rf data
 cp -R DeathMask/data data
 cd data
 rm *.ogg
-mogrify -filter point -resize 320x320\! *.bmp
+mogrify -filter point -resize 512x512\! *.bmp
 cd ..
 source ../emsdk/emsdk_env.sh
 cmake -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake && make
