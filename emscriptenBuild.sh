cbd4edabf07e38101f3c2fa43940e267a2fdf13f
demensdeum <demensdeum@gmail.com>
2018-10-11 11:19:54 +0400

    Ignore emscripten files, credits moved

--- emscriptenBuild.sh
+++ emscriptenBuild.sh
@@ -7,7 +7,6 @@ cp -R DeathMask/data data
 cd data
 rm *.ogg
 rm *.mp3
-mogrify -filter point -resize 512x512\! *.png
 cd ..
 source ../emsdk/emsdk_env.sh
 cmake -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake && make
